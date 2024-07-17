16079844266eb339b70aae79679248a6c2d20e07
ValerioL96 <v.lembo1396@gmail.com>
2024-07-17 20:24:08 +0200

    Created controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/',['PageController@index']);
