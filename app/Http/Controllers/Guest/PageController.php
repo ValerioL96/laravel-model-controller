23d250d2d023663c0751ef5b14c91e25ed601044
ValerioL96 <v.lembo1396@gmail.com>
2024-07-18 10:48:07 +0200

    Added cards in view with bootstrap

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,6 @@ class PageController extends Controller
 {
     public function index(){
 
-
         $movies = Movie::all();
         return view('pages.home', compact('movies'));
     }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,37 @@
 @section('page-title','Laravel Model Controller')
 
 @section('main-content')
-<h1>
-    Movie list
-</h1>
-@dump($movies)
+<section class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>
+                Movie list
+            </h1>
+        </div>
+        @foreach ($movies as $movie )
+        <article class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{$movie->id}} : {{$movie->title}}
+                    </h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">
+                        Titolo originale: {{$movie->original_title}}
+                    </h6>
+                    <p class="card-text">
+                        Nazionalità: {{$movie->nationality}}
+                    </p>
+                    <p class="card-text">
+                        Data di uscita: {{$movie->date}}
+                    </p>
+                    <p class="card-text">
+                        Voto: {{$movie->vote}}
+                    </p>
+                </div>
+            </div>
+        </article>
+        @endforeach
+
+    </div>
+</section>
 @endsection
